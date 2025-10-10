f680c11d22162f4ba0d1f1bb40556f27bca5f430
u210709007 <ilaydagunay46@gmail.com>
2025-10-10 17:29:27 +0300

    week4

---
+++ week4/app/src/main/java/gui/ceng/mu/edu/browser/MainActivity.java
@@ -0,0 +1,56 @@
+package gui.ceng.mu.edu.browser;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import gui.ceng.mu.edu.browser.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnGo;
+    EditText txtAddress;
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnGo = findViewById(R.id.go);
+        txtAddress = findViewById(R.id.adress);
+        webView = findViewById(R.id.webview);
+
+        webView.setWebViewClient(new WebViewClient());
+
+        btnGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                webView.loadUrl("http://"+ txtAddress.getText());
+            }
+        });
+        if (getIntent() !=null && getIntent().getData() != null) {
+            txtAddress.setText(getIntent().getData().toString());
+            webView.loadUrl(getIntent().getData().toString());
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+}
